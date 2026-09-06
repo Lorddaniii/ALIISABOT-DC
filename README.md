8685f0237b7b7996b57231602db7e7afb6c3fcab
Lorddaniii <159727749+Lorddaniii@users.noreply.github.com>
2026-09-06 00:24:04 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,69 @@
+#🔥 𝗔𝗟𝗜𝗦𝗔𝗕𝗢𝗧-𝗗𝗖🔥
+Bot terminado no recibirá más actualizaciones ✨ 
+
+
+
+
+[![blog](https://img.shields.io/badge/YouTube-FF0000?style=for-the-badge&logo=youtube&logoColor=white)
+](https://youtu.be/4eA5nDxPjOA)  <img src="https://github.com/siegrin/siegrin/blob/main/Assets/powerup.gif" height="29px">
+
+###🌱𝐀𝐂𝐓𝐈𝐕𝐀 𝐄𝐋 𝐓𝐄𝐑𝐌𝐔𝐗☘️
+```bash
+┌「𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 𝚃𝙴𝚁𝙼𝚄𝚇」
+├❏pkg update -y && pkg upgrade -y
+├❏pkg install git -y
+├❏pkg install nodejs -y
+├❏pkg install ffmpeg -y
+├❏pkg install imagemagick -y
+├❏pkg install libwebp -y
+├❏termux-setup-storage
+├❏git clone https://github.com/Lorddaniii/ALIISABOT-DC.git
+├❏cd ALISABOT-DC
+├❏npm install
+├❏npm start
+└────ׂ─ׂ─ׂ─ׂ───  
+```
+### `NOTAS`
+```bash
+┌「𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂」
+├❏> Para activar algunos comandos como 
+├❏el #añadir y #sacar el propietario del 
+├❏Bot debera usar el comando #enable restrict 
+├❏desde el numero que haya puesto en el archivo 
+├❏config.js
+┌「𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂」
+├❏> Para obtener nuevamente el codigo QR, escribe en el termux:
+├❏> cd ALISABOT-DC
+├❏> rm -rf ALISABOT-DC-Session
+├❏> npm start 
+└────ׂ─ׂ─ׂ─ׂ───
+┌「𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂」
+├❏> Si el termux se cierra para volver activar escribe:
+├❏> cd ALISABOT-DC
+├❏> npm start 
+└────ׂ─ׂ─ׂ─ׂ───
+├❏> Cada vez que realices una modificacion en el
+repositorio del Bot (tu GitHub), puedes usar 
+el comando #actualizar para que se actualicen los datos
+└────ׂ─ׂ─ׂ─ׂ───
+├❏> Aconsejable maximo 30 grupos, despues 
+de esa cantidad el Bot empieza a ir 
+excesivamente lento (depende del WhatsApp igual)
+└────ׂ─ׂ─ׂ─ׂ───
+```
+### `AJUSTES`
+- CAMBIAR NÚMERO DE PROPIETARIO [Aqui](https://github.com/Lorddaniii/ALIISABOT-DC/blob/master/config.js)
+> ##### WhatsApp recomendado por el creador del Bot
+* [Opción 1 - WhatsApp Recomendado](https://www.mediafire.com/file/o80pni1rfi0n6zz/ʙᴜsͨɪͧɴᷨᴇͣs๋ᷡsͦ_V6_PRIMARIO.apk/file).
+
+* [Opción 2 - WhatsApp Recomendado](https://www.mediafire.com/file/4kkiwpxvciabjf5/ʙᴜsͨɪͧɴᷨᴇͣs๋ᷡsͦ_V6_SECUNDARIO.apk/file).
+## `PORPIETARIO DEL BOT` 
+[![OFC FRANKLIN](https://i.imgur.com/lkch77g.jpeg)](https://github.com/Lorddaniii/ALIISABOT-DC) 
+```bash
+
+
+
+
+
+`OFC FRANKLIN___ALISABOT-DC"` 
+```
---
+++ config.js
@@ -0,0 +1,146 @@
+import fs from 'fs'
+import { watchFile, unwatchFile } from 'fs'
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+
+global.owner = [
+  ['51901619079', 'OFC FRANKLIN', true],
+  ['51935269321', 'OFC FRANKLIN', true],
+  ['51951939243', 'OFC FRANKLIN', true],
+]
+
+global.mods = ['51951939243'],
+global.prems = ['51901619079', '51935269321', '51951939243']
+global.APIs = { 
+
+  amel: 'https://melcanz.com',
+  bx: 'https://bx-hunter.herokuapp.com',
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  nzcha: 'http://nzcha-apii.herokuapp.com',
+  bg: 'http://bochil.ddns.net',
+  fdci: 'https://api.fdci.se',
+  dzx: 'https://api.dhamzxploit.my.id',
+  bsbt: 'https://bsbt-api-rest.herokuapp.com',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.me',
+  hardianto: 'https://hardianto-chan.herokuapp.com',
+  pencarikode: 'https://pencarikode.xyz', 
+  LeysCoder: 'https://leyscoders-api.herokuapp.com',
+  adiisus: 'https://adiixyzapi.herokuapp.com',
+  lol: 'https://api.lolhuman.xyz',
+  pencarikode: 'https://pencarikode.xyz',
+  Velgrynd: 'https://velgrynd.herokuapp.com',
+  rey: 'https://server-api-rey.herokuapp.com',
+  hardianto: 'http://hardianto-chan.herokuapp.com',
+  shadow: 'https://api.reysekha.xyz',
+  apialc: 'https://api-alc.herokuapp.com',
+  botstyle: 'https://botstyle-api.herokuapp.com',
+  neoxr: 'https://neoxr-api.herokuapp.com',
+  ana: 'https://anabotofc.herokuapp.com/',
+  kanx: 'https://kannxapi.herokuapp.com/',
+  dhnjing: 'https://dhnjing.xyz'
+},
+  
+global.APIKeys = { 
+  'https://api-alc.herokuapp.com': 'ConfuMods',
+  'https://api.reysekha.xyz': 'apirey',
+  'https://melcanz.com': 'F3bOrWzY',
+  'https://bx-hunter.herokuapp.com': 'Ikyy69',
+  'https://api.xteam.xyz': '5bd33b276d41d6b4',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://bsbt-api-rest.herokuapp.com': 'benniismael',
+  'https://api.zeks.me': 'apivinz',
+  'https://hardianto-chan.herokuapp.com': 'hardianto',
+  'https://pencarikode.xyz': 'pais', 
+  'https://leyscoders-api.herokuapp.com': 'MIMINGANZ', 
+  'https://server-api-rey.herokuapp.com': 'apirey',
+  'https://api.lolhuman.xyz': '9b817532fadff8fc7cb86862',
+  'https://botstyle-api.herokuapp.com': 'Eyar749L',
+  'https://neoxr-api.herokuapp.com': 'yntkts',
+  'https://anabotofc.herokuapp.com/': 'AnaBot'
+}
+
+global.botName = 'ALISABOT-DC'
+global.packname = 'ALISABOT-DC﹏✍'
+global.author = '© ALISABOT-DC﹏✍'
+
+global.vs = '1.0.30'
+global.version = vs
+
+global.gt = 'ALISABOT-DC'
+global.botBrand = gt
+
+global.yt = 'https://youtu.be/aPu9wQi-z8U'
+global.youtube = yt
+
+global.ig = 'https://www.instagram'
+global.instagram = ig
+
+global.md = 'https://github.com/Lorddaniii/ALIISABOT-DC'
+global.repository = md
+
+global.nn = 'https://chat.whatsapp.com/F22dCa3Ly5rGj07YV51oZ1'
+global.nngrupo = nn
+
+global.nnn = nn
+global.nnngrupo = nnn
+
+global.paypal = 'https://pay'
+global.donar = paypal
+
+global.rg = '*🍀ʀᴇsᴜʟᴛᴀᴅᴏ ᴇɴᴄᴏɴᴛʀᴀᴅᴏ✨*'
+global.resultado = rg
+
+global.ag = '*⚠️ᴀᴅᴠᴇʀᴛᴇɴᴄɪᴀ⚠️*'
+global.advertencia = ag
+
+global.iig = '*📌ɪɴғᴏʀᴍᴀᴄɪᴏɴ*✨'
+global.informacion = iig
+
+global.fg = '*❌ʟᴏ sᴇɴᴛɪᴍᴏs sᴇ ʜᴀ ɢᴇɴᴇʀᴀᴅᴏ ᴜɴ ᴇʀʀᴏʀ ᴠᴜᴇʟᴠᴇ ɪɴᴛᴇɴᴛᴀʀ❌*'
+global.fallo = fg
+
+global.mg = '*❗ᴇsᴛᴇ ᴄᴏᴍᴀɴᴅᴏ ʟᴏ ʜᴀ ᴜsᴀᴅᴏ ɪɴᴄᴏʀʀᴇᴄᴛᴀᴍᴇɴᴛᴇ*'
+global.mal = mg
+
+global.eeg = '*📩ˢᵁ ᴿᴱᴾᴼᴿᵀᴱ ᴴᴬ ˢᴵᴰᴼ ᴱᴺⱽᴵᴬᴰᴼ📩*'
+global.envio = eeg
+
+global.eg = '*🍀ʀᴇsᴜʟᴛᴀᴅᴏ ᴇɴᴄᴏɴᴛʀᴀᴅᴏ✨*'
+global.exito = eg
+
+global.wm = '© OFC FRANKLIN\nALISABOT-DC'
+global.igfg = 'ALISABOT-DC'
+global.wait = '⌛ _Cargando..._\n▰▰▰▱▱▱▱▱▱'
+
+global.imagen1 = fs.readFileSync('./src/Menu2.jpg')
+global.imagen4 = fs.readFileSync('./src/Menuxx.jpg')
+global.imagen2 = fs.readFileSync('./src/nuevobot.jpg') 
+global.imagen3 = fs.readFileSync('./src/Me.jpg')
+
+
+global.multiplier = 9999 
+
+global.rpg = {
+  emoticon(string) {
+    string = string.toLowerCase()
+    let emot = {
+      level: '🏆',
+      limit: '💎',
+      exp: '🕹️'
+    }
+    let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+    if (!results.length) return ''
+    else return emot[results[0][0]]
+  }
+}
+
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
+ 
